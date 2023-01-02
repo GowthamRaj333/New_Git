7083af54b558271411a646926db64385e1f2fd7b
GowthamRaj333 <ranjithgowthamraj333@gmail.com>
2023-01-02 19:57:11 +0530

    read

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Sample {
+	
+	public void m1() {
+		System.out.println("one");
+	}
+	
+	public void m2() {
+		System.out.println("two");
+	}
+	
+	public void m3() {
+		System.out.println("three");
+	}
+
+}
